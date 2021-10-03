9505f4ded87321b552f9bbb3ef2bc0c3458b268a
Arnaud de Mouhy <arnaud@admds.net>
2021-10-03 13:03:02 +0200

    reformat using black

--- create_labels.py
+++ create_labels.py
@@ -1,11 +1,12 @@
 import cv2
-path_to_new_labels = ''    
-if path_to_new_labels==False:
-    print('Path to new labels missing')
-    
+
+path_to_new_labels = ""
+if path_to_new_labels == False:
+    print("Path to new labels missing")
+
 alls = os.listdir(path_to_labels)
 for al in alls:
-    GT = cv2.imread(path_to_labels+al,0)
-    GT[GT!=255]=0
-    cl = cv2.imwrite(path_to_new_labels+al,GT)
-    print('Label written')
+    GT = cv2.imread(path_to_labels + al, 0)
+    GT[GT != 255] = 0
+    cl = cv2.imwrite(path_to_new_labels + al, GT)
+    print("Label written")
